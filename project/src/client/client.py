889168b08d63f8378679810584cf16f1bf2876ec
mpakulsk <01169219@pw.edu.pl>
2024-01-13 18:56:40 +0100

    Replace client.py

--- project/src/client/client.py
+++ project/src/client/client.py
@@ -1,13 +1,14 @@
 import socket
 import json
 import argparse
+import os
 
-
-def send_request(server_host, server_port, command, path=""):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((server_host, server_port))
+def send_request(client_socket, command, path=""):
         request = json.dumps({'command': command, 'path': path})
         client_socket.send(request.encode())
+
+        if command == "exit":
+            return
 
         response = receive_json_response(client_socket)
         if response["status"] == "error":
@@ -22,6 +23,9 @@ def send_request(server_host, server_port, command, path=""):
             elif command == 'get':
                 if "size" in response:
                     save_path = input("Podaj nazwę pliku do zapisu (opcjonalnie ze ścieżką): ")
+                    while os.path.isdir(save_path):
+                        print("Niepoprawna ścieżka: wskazuje na istniejący folder")
+                        save_path = input("Podaj nazwę pliku do zapisu (opcjonalnie ze ścieżką): ")
                     receive_file_data(client_socket, save_path, response["size"])
                 else:
                     print("Brak danych pliku do pobrania.")
@@ -47,27 +51,31 @@ def receive_file_data(client_socket, save_path, file_size):
 
 
 def run_client(server_host, server_port):
-    while True:
-        print("Dostępne komendy: get, ls, tree, exit")
-        command = input("Wprowadź komendę: ")
-
-        if command == 'exit':
-            break
-        elif command in ['ls', 'tree']:
-            path = input("Wprowadź ścieżkę: ")
-            send_request(server_host, server_port, command, path)
-        elif command == 'get':
-            path = input("Wprowadź ścieżkę do pliku: ")
-            send_request(server_host, server_port, command, path)
-        else:
-            print("Nieznana komenda.")
-        print("\n")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_host, server_port))
+        
+        while True:
+            print("Dostępne komendy: get, ls, tree, exit")
+            command = input("Wprowadź komendę: ")
+            
+            if command == 'exit':
+                send_request(client_socket, command)
+                break
+            elif command in ['ls', 'tree']:
+                path = input("Wprowadź ścieżkę: ")
+                send_request(client_socket, command, path)
+            elif command == 'get':
+                path = input("Wprowadź ścieżkę do pliku: ")
+                send_request(client_socket, command, path)
+            else:
+                print("Nieznana komenda.")
+            print("\n")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('host', help='Server host')
-    parser.add_argument('--port', default=65536, type=int, help='Server port')
+    parser.add_argument('--port', default=65432, type=int, help='Server port')
     args = parser.parse_args()
     run_client(args.host, args.port)
 
